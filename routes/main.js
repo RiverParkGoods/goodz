0a83f64899adeb038824660454f5ec5ffc31b13c
tonypark-cw <qkrcksdna@naver.com>
2023-04-26 14:48:12 +0900

    0426:BackEnd:main.js with mongoose

--- routes/main.js
+++ routes/main.js
@@ -37,17 +37,13 @@ app.get('/greeting', (req, res) => {
    res.send('Welcome 😊');
 });
 
-app.get('select', (req, res) => {
-   resselect_result(req, res);
-});
-
 app.get('/product/list', (req, res) => {
    const result = mysql_conn.query('select * from product;');
    console.log(result.length);
    res.writeHead(200);
    let temp = '';
    for (let i = 0; i < result.length; i++) {
-      temp += `<tr><td>${result[i].prodId}</td><td>${result[i].prodName}</td><td>${result[i].prodPrice}</td><td>${result[i].prodArti}</td></tr>`;
+      temp += `<tr><td>${result[i].prodId}</td><td>${result[i].prodName}</td><td>${result[i].prodPrice}</td><td>${result[i].prodArti}</td><td>${result[i].prodImg}</td></tr>`;
    }
    let resultPage = makeResultTemplate(` 
    <table border="1" style="margin: auto; text-align: center">
@@ -57,6 +53,7 @@ app.get('/product/list', (req, res) => {
             <th>PRODUCT NAME</th>
             <th>PRODUCT PRICE</th>
             <th>PRODUCT ARTIST</th>
+            <th>PRODUCT IMAGE</th>
          </tr>
       </thead>
       <tbody>
@@ -67,18 +64,27 @@ app.get('/product/list', (req, res) => {
 });
 
 app.get('/product/search', (req, res) => {
-   console.log(req.query);
+   // 검색 조건 - 검색할때, 상품ID, 상품이름, 아티스트이름 으로 각각 골라 검색하거나, 모두다 검색하는지 선택해서 searchway 로 넘겨줌.
    const searchway = req.query.searchway;
+   // 검색 단어
    const keyword = req.query.keyword;
+   // query 검색 후 나오는 결과.
    let result;
-   console.log(searchway, keyword);
 
+   //검색조건은 select-option이라 null인 경우가 없는게 정상이지만, 주소로 그냥 접근하는 경우 비어있을수 있고, 검색단어를 빈상태로 검색을 하는경우에 오류가 발생할테니 사전에 error handling
+   if (searchway == '') {
+      res.write('<script>alert("검색조건이 올바르지 않습니다.");</script>');
+      res.redirect('/product/list');
+   } else if (keyword == '') {
+      res.write('<script>alert("검색단어가 들어있지 않습니다.");</script>');
+      res.redirect('/product/list');
+   }
+
+   // 검색 조건에 따라 다른 쿼리를 작성해 select 수행.
    if (searchway == 'prodId') {
       result = mysql_conn.query('select * from product where prodId=?;', [keyword]);
    } else if (searchway == 'prodName') {
       result = mysql_conn.query('select * from product where prodName like ?;', ['%' + keyword + '%']);
-      console.log('여기');
-      console.log(result);
    } else if (searchway == 'prodArti') {
       result = mysql_conn.query('select * from product where prodArti like ?;', ['%' + keyword + '%']);
    } else if (searchway == 'any') {
@@ -87,67 +93,83 @@ app.get('/product/search', (req, res) => {
          '%' + keyword + '%',
          '%' + keyword + '%',
       ]);
-   } else {
-      res.redirect('/product/list');
    }
-   console.log(result);
 
+   //결과로 보여지는 테이블을 담기위해 resultPage 변수 생성
    let resultPage = ``;
+   //result가 있는 경우에만 table로 찍고, 없는 경우엔 조회결과가 없음을 띄운다.
    if (result.length > 0) {
       let temp = '';
-      console.log(result.length);
       res.writeHead(200);
-      res.end('{"ok":true, "product":' + JSON.stringify(result) + '}');
-      // for (let i = 0; i < result.length; i++) {
-      //    temp += `<tr><td>${result[i].prodId}</td><td>${result[i].prodName}</td><td>${result[i].prodPrice}</td><td>${result[i].prodArti}</td></tr>`;
-      // }
-      // resultPage = makeResultTemplate(`
-      //    <table border="1" style="margin: auto; text-align: center">
-      //       <thead>
-      //          <tr>
-      //             <th>PRODUCT ID</th>
-      //             <th>PRODUCT NAME</th>
-      //             <th>PRODUCT PRICE</th>
-      //             <th>PRODUCT ARTIST</th>
-      //          </tr>
-      //       </thead>
-      //       <tbody>
-      //          ${temp}
-      //       </tbody>
-      //    </table>
-      // `);
-      // res.end(resultPage);
+      // res.end('{"ok":true, "product":' + JSON.stringify(result) + '}');
+      for (let i = 0; i < result.length; i++) {
+         temp += `<tr><td>${result[i].prodId}</td><td>${result[i].prodName}</td><td>${result[i].prodPrice}</td><td>${result[i].prodArti}</td><td>${result[i].prodImg}</td></tr>`;
+      }
+      resultPage = makeResultTemplate(`
+         <table border="1" style="margin: auto; text-align: center">
+            <thead>
+               <tr>
+                  <th>PRODUCT ID</th>
+                  <th>PRODUCT NAME</th>
+                  <th>PRODUCT PRICE</th>
+                  <th>PRODUCT ARTIST</th>
+                  <th>PRODUCT IMAGE</th>
+               </tr>
+            </thead>
+            <tbody>
+               ${temp}
+            </tbody>
+         </table>
+      `);
+      res.end(resultPage);
    } else {
       resultPage = makeResultTemplate(`
             <div class="empty">
                Can't find Any Data.
             </div> `);
       // res.write('<script>alert("조회 결과가 없어요.");</script>');
-      res.send(
-         '{"ok":false, "product":{"prodId":undefined, "prodName":undefined, "prodPrice":undefined,"prodArti":undefined}}',
-      );
+      // res.send(
+      //    '{"ok":false, "product":{"prodId":undefined, "prodName":undefined, "prodPrice":undefined,"prodArti":undefined, "prodImg":undefined}}',
+      // );
+      res.end(resultPage);
    }
 });
 
 app.post('/product/insert', (req, res) => {
+   // 상품ID, 상품이름, 상품가격, 상품의아티스트 정보를 request.body에서 뽑아냄.
    const { prodId, prodName, prodPrice, prodArti } = req.body;
-   if (prodId == '') {
-      res.write("<script>alert('Product ID 입력해주세요.');</script>");
+   // 사진 정보를 추가 할 것 이어서, 데이터 확인을 위해서만 default.png로 일괄 지정
+   let prodImg = 'default.png';
+   // 입력 값들이 들어있지 않은 경우에 대한 error handling
+   if (prodId == '' || prodName == '' || prodPrice == '' || prodArti == '') {
+      res.write("<script>alert('모든 상품 정보를 입력해주세요.');</script>");
    } else {
-      // res.write('<script>confirm("상품 정보를 추가할까요?");</script>');
-      const result = mysql_conn.query('insert into product values(?,?,?,?);', [prodId, prodName, prodPrice, prodArti]);
+      // insert query 작성후 결과를 result로.
+      const result = mysql_conn.query('insert into product values(?,?,?,?,?);', [
+         prodId,
+         prodName,
+         prodPrice,
+         prodArti,
+         prodImg,
+      ]);
+      //추후에 페이지에 추가된 결과로 보여지게 res.redirect를 활용할 예정
       if (result.affectedRows == 1) {
-         res.send('{"ok":true, "affectedRows":' + result.affectedRows + ', "service":"insert"}');
+         res.send('{"ok":true, "db":"mysql", "service":"product/insert"}');
       } else {
-         res.send('{"ok":false, "affectedRows":' + result.affectedRows + ', "service":"insert"}');
+         res.send('{"ok":false, "db":"mysql", "service":"product/insert"}');
       }
    }
 });
 
 app.post('/product/update', (req, res) => {
+   // 상품ID, 상품이름, 상품가격, 상품의아티스트 정보를 request.body에서 뽑아냄.
    const { prodId, prodName, prodPrice, prodArti } = req.body;
+   // 수정할 값에 대해 공백으로 입력하면 원본을 유지, 입력해준 값들만 새롭게 변경할 예정이라,
+   // 기본적으로 공통적인 query를 변수로 지정.
    let query = 'update product set ';
+   // query에 이어 붙이기 위해 배열로 지정, 추후에 join을 통해 ','로 이어 붙일 예정
    let q_arr = [];
+   // conn.query의 두번째 파라미터로 ?에 해당하는 변수들을 배열로 지정.
    let q_var = [];
 
    if (prodName) {
@@ -164,14 +186,17 @@ app.post('/product/update', (req, res) => {
    }
    if (prodId) {
       q_var.push(prodId);
+
       query += q_arr.join(',');
+      query += 'where prodId=?';
+
       // res.write('<script>confirm("상품을 수정할까요?");</script>');
       const result = mysql_conn.query(query, q_var);
       // res.write('<script>window.location="/product/list"</script>');
       if (result.affectedRows == 1) {
-         res.send('{"ok":true, "affectedRows":' + result.affectedRows + ', "service":"update"}');
+         res.send('{"ok":true, "db":"mysql", "service":"product/update"}');
       } else {
-         res.send('{"ok":false, "affectedRows":' + result.affectedRows + ', "service":"update"}');
+         res.send('{"ok":false, "db":"mysql", "service":"product/update"}');
       }
    } else {
       res.write('<script>alert("PRODUCT ID를 입력해주세요.");</script>');
@@ -185,58 +210,129 @@ app.post('/product/delete', (req, res) => {
       const result = mysql_conn.query('delete from product where prodId=?', [prodId]);
       // res.write('<script>window.location="/product/list"</script>');
       if (result.affectedRows == 1) {
-         res.send('{"ok":true, "affectedRows":' + result.affectedRows + ', "service":"delete"}');
+         res.send('{"ok":true, "db":"mysql", "service":"product/delete"}');
       } else {
-         res.send('{"ok":false, "affectedRows":' + result.affectedRows + ', "service":"delete"}');
+         res.send('{"ok":false, "db":"mysql", "service":"product/delete"}');
       }
    } else {
       res.write('<script>alert("Product ID를 입력해주세요.");</script>');
    }
 });
-321;
+
 app.get('/error', (req, res) => {
    res.redirect('error.html');
 });
 
-// const mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-// //order 정보 수정 예정.
-// let orderSchema = mongoose.Schema(
-//    {
-//       orderIdx: String,
-//       orderNumber: String,
-//       products: Map,
-//       of: String,
-//       orderAmount: String,
-//       orderTotPrice: Number,
-//    },
-//    {
-//       versionKey: false,
-//    },
-// );
+let orderSchema = mongoose.Schema(
+   {
+      orderIdx: String,
+      orderNumber: String,
+      prodId: String,
+      orderAmount: String,
+      orderTotPrice: Number,
+   },
+   {
+      versionKey: false,
+   },
+);
 
-// let User = mongoose.model('order', orderSchema);
-// app.get('/Hello', (req, res) => {
-//    res.send('Hello World');
-//    return;
-// });
+let Order = mongoose.model('orders', orderSchema);
 
-// app.post('/insert', (req, res, next) => {
-//    let userid = req.body.userid;
-//    let name = req.body.name;
-//    let city = req.body.city;
-//    let gender = req.body.gender;
-//    let age = req.body.age;
-//    let user = new User({ userid: userid, name: name, city: city, gender: gender, age: age });
-//    user.save((err, silence) => {
-//       if (err) {
-//          console.log(err);
-//          res.status(500).send('insert error');
-//          return;
-//       }
-//       res.status(200).send('Inserted');
-//       return;
-//    });
-// });
+app.get('/order/list', (req, res, next) => {
+   Order.find({}, (err, docs) => {
+      if (err) console.log(err);
+      res.send(docs);
+      // res.send(docs);
+      return;
+   });
+   return;
+});
+
+app.get('/order/search', (req, res, next) => {
+   const searchway = req.query.searchway.toString();
+   const keyword = req.query.keyword.toString();
+   if (searchway == 'orderIdx') {
+      Order.findOne({ orderIdx: keyword }, (err, docs) => {
+         if (err) console.log(err);
+         // console.log(docs);
+         res.send({ order: docs });
+         return;
+      });
+   } else if (searchway == 'orderNumber') {
+      Order.find({ orderNumber: keyword }, (err, docs) => {
+         if (err) console.log(err);
+         // console.log(docs);
+         res.send({ order: docs });
+         return;
+      });
+   }
+
+   console.log(req.query.input);
+
+   return;
+});
+
+app.post('/order/insert', (req, res, next) => {
+   let orderIdx = req.body.orderIdx;
+   let orderNumber = req.body.orderNumber;
+   let prodId = req.body.prodId;
+   let orderAmount = req.body.orderAmount;
+   let orderTotPrice = req.body.orderTotPrice;
+   let order = new Order({
+      orderIdx: orderIdx,
+      orderNumber: orderNumber,
+      prodId: prodId,
+      orderAmount: orderAmount,
+      orderTotPrice: orderTotPrice,
+   });
+   order.save((err, silence) => {
+      if (err) {
+         res.status(500).send({ ok: false, db: 'mongodb', service: 'order/insert' });
+         return;
+      }
+      res.status(200).send({ ok: true, db: 'mongodb', service: 'order/insert' });
+      return;
+   });
+});
+
+app.post('/order/update', (req, res, next) => {
+   let orderIdx = req.body.orderIdx;
+   let orderNumber = req.body.orderNumber;
+   let prodId = req.body.prodId;
+   let orderAmount = req.body.orderAmount;
+   let orderTotPrice = req.body.orderTotPrice;
+
+   Order.findOne({ orderIdx: orderIdx }, (err, order) => {
+      order.orderNumber = orderNumber;
+      order.prodId = prodId;
+      order.orderAmount = orderAmount;
+      order.orderTotPrice = orderTotPrice;
+      order.save((err, silence) => {
+         if (err) {
+            console.log(err);
+            res.status(500).send({ ok: false, db: 'mongodb', service: 'order/update' });
+            return;
+         }
+         res.status(200).send({ ok: true, db: 'mongodb', service: 'order/update' });
+         return;
+      });
+   });
+});
+
+app.post('/order/delete', (req, res, next) => {
+   let orderIdx = req.body.orderIdx;
+   Order.findOne({ orderIdx: orderIdx }, (err, order) => {
+      order.deleteOne(err => {
+         if (err) {
+            res.status(500).send({ ok: false, db: 'mongodb', service: 'order/delete' });
+            return;
+         }
+         res.status(200).send({ ok: true, db: 'mongodb', service: 'order/delete' });
+         return;
+      });
+   });
+});
 
 module.exports = app;
